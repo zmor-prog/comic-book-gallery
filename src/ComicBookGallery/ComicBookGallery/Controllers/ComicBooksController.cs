4e3b98c33ef4d87d0bdbca8d1be9bbc1ea1da49e
zmor-prog <zmorgan@nyit.edu>
2020-06-06 21:06:34 -0400

    Added Comic Book Detail view

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -11,15 +11,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail() 
         {
-
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                return Redirect("/");
-                
-            }
-
-            return Content("Hello from the comic books controller!");
-        
+            return View();
         }
     }
 }
