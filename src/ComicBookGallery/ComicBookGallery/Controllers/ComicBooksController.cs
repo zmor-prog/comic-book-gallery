30d927adc445fbc5a755b23047bfb9c77d8d5449
zmor-prog <zmorgan@nyit.edu>
2020-12-30 10:57:26 -0500

    Added Comic Books List view

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -17,6 +17,14 @@ namespace ComicBookGallery.Controllers
         {
             _comicBookRepository = new ComicBookRepository();
         }
+
+        public ActionResult Index()
+        {
+            var comicBooks = _comicBookRepository.GetComicBooks();
+
+            return View(comicBooks);
+        }
+
         public ActionResult Detail(int? id) 
         {
             if (id == null)
